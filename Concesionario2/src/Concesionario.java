d00ba5e1bffe4178024635c959651510a32801af
raulmunoztome <raulmunoztome@gmail.com>
2026-03-01 02:02:20 +0100

    fix v2

--- Concesionario2/src/Concesionario.java
+++ Concesionario2/src/Concesionario.java
@@ -13,9 +13,8 @@ public class Concesionario implements ConcessionariOperacions{
 	 
 	public Concesionario(String direccion,List<Vehicle> listaVehiculos) throws Exception {
 		this.direccion = direccion;
-		if(cantidad == MAX_VEHICULOS) throw new Exception("Máximo de vehiculos alcanzado");
-		++cantidad;
 		this.elementos = new TreeSet<>(listaVehiculos);
+		++cantidad;
 
 	}
 	
@@ -49,42 +48,61 @@ public class Concesionario implements ConcessionariOperacions{
 	public int getCantidad() {
 		return this.cantidad;
 	}
+	
 	public TreeSet<Vehicle> getElementos() {
 		return elementos;
 	}
+	
 	public boolean addVehiculo(Vehicle vc) {
+		
 		if(cantidad == MAX_VEHICULOS) return false;
+		
 		elementos.add(vc);
 		++cantidad;
+		
 		return true;
 	}
 	
 	public int mediaKm() {
+		
 		int media = 0;
 		Iterator<Vehicle> revisar = elementos.iterator();
 		
 		while(revisar.hasNext()) {
+			
 			media += revisar.next().getKm();
 		}
 		
 		return media/elementos.size();
 	}
+	
 	public double descuento(Vehicle v) {
 		
 		if(v instanceof Cotxe) {
+			
 			if(v.isEsAutomatic()) return 0;
+			
 			int dato = v.getKm();
+			
 			if((dato/10000) > 10 )return 10;
+			
 			return dato/1000;
 		}
+		
 		else {
 			if(v.getCilindrada() > 125) {
+				
 				int dato = v.getKm();
+				
 				if((dato/10000) > 10 )return 10;
+				
 				return dato/1000;
 			}
+			
 			int motokm = v.getKm();
+			
 			if((motokm/5000)*2 > 20) return 20;
+			
 			return (v.getKm()/5000)*2;
 		}
 		
